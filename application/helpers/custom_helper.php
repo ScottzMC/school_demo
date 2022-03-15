83240b27228c9fe595e201e6327ed85810ab8810
Rising Phoenix <scottmike275@gmail.com>
2022-03-15 11:04:33 +0000

    Update custom_helper.php

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -1,14 +1,10 @@
 <?php 
 
 	function show_message($message, $opp){
-
-		if($message == "success" && $opp == "add"){
-			echo "<script>alert('Added successfully');</script>";
-		}else if($message == "success" && $opp == "edit"){
-			echo "<script>alert('Updated successfully');</script>";
-		}else if($message == "failed"){
-			echo "<script>alert('Failed successfully');</script>";
-		}
+		if($message == "success" && $opp == "add") echo "<script>alert('Added successfully');</script>";
+		if($message == "success" && $opp == "edit") echo "<script>alert('Updated successfully');</script>";
+		if($message == "success" && $opp == "require") echo "<script>alert('Please select a school');</script>";
+		if($message == "failed") echo "<script>alert('Failed successfully');</script>";
 	}
 
 ?>
