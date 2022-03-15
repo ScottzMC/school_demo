7c1fe910b9d3a670ec33a758f6b237b3231becdd
Rising Phoenix <scottmike275@gmail.com>
2022-03-15 13:50:26 +0000

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'School';
+$route['default_controller'] = 'School/view';
 $route['404_override'] = 'custom_error';
 $route['translate_uri_dashes'] = FALSE;
